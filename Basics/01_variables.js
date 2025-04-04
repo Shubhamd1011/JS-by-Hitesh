e4069d6521c20af28dcbfa9d83ae303dee146178
Shubham d <shubhamdevkamble707@gmail.com>
2025-04-04 12:57:13 +0530

    variables

---
+++ Basics/01_variables.js
@@ -0,0 +1,14 @@
+/* Theire are 2 ways to declare the veriable 
+1.constants => const
+2.variables => var and let
+
+1.const : this are not changble or not update when we declare them they cant be redcale or reasign.
+2.let : It is a block scope it can be update but not reasign.
+3.var : It allows to redeclare the variable and update them.
+
+exaples are as follows
+*/
+
+let name = "Shubham"
+var lName = "Devkamble"
+const dob = 10;
