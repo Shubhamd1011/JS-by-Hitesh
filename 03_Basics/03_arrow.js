c127519588b3fc314fc76e82a8d36a21ad33f024
Shubham d <shubhamdevkamble707@gmail.com>
2025-05-07 00:03:20 +0530

    Arrow Function

--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -42,3 +42,26 @@ const arrow = ()=>{
   console.log("Hello im Arrow function"); 
 }
 arrow(); // Like that we execute the arrow function
+
+// const addTwo = (num1, num2) =>{
+//   return num1 + num2
+// }
+// console.log(addTwo(1,3));
+
+// When we have only 1 line function on that time we use implesite return which means we dont have to need write return keyword it automatically assume that it was the return ketword and we dont have to need use this {} for example,
+
+const addTwo = (num1, num2) =>  num1 + num2; // This is the example of implesite return.
+console.log(addTwo(2,3)); 
+// Key Note : when we wrap the function {} on that time we need to add return keyword but when we use () on that time we dont have to need the return it means it is a implesite return.
+
+const addThree = (num1, num2, num3) => (num1 + num2 + num3)
+console.log(addThree(10,11,12)); 
+
+// When we have to add objects in the arrow function on that time.
+// In ({in that we write the objects})
+const getUser = () => ({userName : "Shubham"})
+console.log(getUser());
+
+
+// Explesite return means our regular return function.
+
