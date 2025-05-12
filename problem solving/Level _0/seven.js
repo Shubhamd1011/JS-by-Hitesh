667016713a7d322529a80dde8c1c93830a037b02
Shubham d <shubhamdevkamble707@gmail.com>
2025-05-13 00:58:14 +0530

    Sum of first n numbers

---
+++ problem solving/Level _0/seven.js
@@ -0,0 +1,13 @@
+/*
+Problem 7: Calculate the Sum of First n Natural Numbers
+Description: Write a program to calculate the sum of first n natural numbers.
+*/
+ let sum = 0;
+ let n = 5;
+ for (i = 1; i <= n; i++){
+   sum += i;
+ }
+ console.log(`The sum of first ${n} num is ${sum}`);
+
+
+
