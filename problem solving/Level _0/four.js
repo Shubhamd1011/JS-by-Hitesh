6fd977c7998087c9482e06976e2857a7ce7b1caf
Shubham d <shubhamdevkamble707@gmail.com>
2025-05-13 00:25:08 +0530

    Even or odd

---
+++ problem solving/Level _0/four.js
@@ -0,0 +1,11 @@
+/*
+Problem 4: Check if a Number is Even or Odd
+Description: Write a program that checks whether a given number is even or odd.
+*/
+
+let num = 10;
+if (num % 2 === 0){
+  console.log(`${num} is a Even number`);
+}else{
+  console.log(`${num} is odd Number`);
+}
