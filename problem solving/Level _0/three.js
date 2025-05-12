99ba1548f250c33f09d6a163633ade9181ef7e03
Shubham d <shubhamdevkamble707@gmail.com>
2025-05-13 00:20:39 +0530

    Swapped two veriables

---
+++ problem solving/Level _0/three.js
@@ -0,0 +1,11 @@
+/*
+ Problem 3: Swap Two Numbers
+Description: Write a program to swap the values of two variables.
+ */
+let a = 3;
+let b = 8;
+let c = a;
+a = b;
+b = c;
+
+console.log(`The swaped value of a is ${a} and b is ${b}`);
