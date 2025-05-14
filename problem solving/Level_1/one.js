9cd5336574e3ee757eef58f477e0dca683e159cb
Shubham d <shubhamdevkamble707@gmail.com>
2025-05-14 23:20:16 +0530

    Factorial of Number

--- problem solving/Level_1/one.js
+++ problem solving/Level_1/one.js
@@ -1,2 +1,11 @@
 // Problem 9: Factorial of a Number
 // Description: Write a program to find the factorial of a number n.
+
+let n = 5;
+let fact = 1;
+
+for(let i = 1; i <=n; i++){
+  fact = fact * i;
+};
+console.log(`Factorial of ${n} is ${fact}`);
+
