8c22e89dfa349918de78f379b291306bacb161f5
Shubham d <shubhamdevkamble707@gmail.com>
2025-05-14 23:50:39 +0530

    Reverse a string

---
+++ problem solving/Level_1/three.js
@@ -0,0 +1,10 @@
+/*
+Task: Reverse a String and Capitalize the First Letter
+Description:
+Write a program that takes a string, reverses it, and then capitalizes the first letter of the reversed string.
+For example:
+
+Input: "hello"
+
+Output: "Olleh"
+ */
